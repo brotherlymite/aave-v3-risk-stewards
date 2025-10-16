9e1cf5de47fae3cfa4f34683dbb7768dce924c4f
Harsh Pandey <harshsatishpandey@gmail.com>
2025-10-16 14:34:43 +0530

    Add risk update for AaveV3Ethereum - CapsUpdates ()

---
+++ src/contracts/updates/CapsUpdates/config.ts
@@ -0,0 +1,20 @@
+import {ConfigFile} from '../../generator/types';
+
+export const config: ConfigFile = {
+  rootOptions: {
+    configFile: 'src/contracts/updates/20251016_AaveV3Ethereum_CapsUpdates/config.ts',
+    force: true,
+    pools: ['AaveV3Ethereum'],
+    title: 'CapsUpdates',
+    shortName: 'CapsUpdates',
+    date: '20251016',
+    author: 'brotherlymite',
+    discussion: '',
+  },
+  poolOptions: {
+    AaveV3Ethereum: {
+      configs: {CAPS_UPDATE: [{asset: 'WETH', supplyCap: '10', borrowCap: '10'}, {asset: 'USDC', supplyCap: '5', borrowCap: '5'}]},
+      cache: { },
+    },
+  },
+};
